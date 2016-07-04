a5be03273d9f156bd2a8a1f08d52ccab00d6f60f
qiyq <ak_qyq@qq.com>
2016-07-04 17:15:21 +0800

    node

---
+++ models/db.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Evan on 2016/7/4.
+ */
+var settings = require('../setting'),
+    Db = require('mongodb').Db,
+    Connection = require('mongodb').Connection,
+    Server = require('mongodb').Server;
+    module.exports = new Db(settings.db, new Server(settings.host, settings.port),
+    {safe: true});
---
+++ setting.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Evan on 2016/7/4.
+ */
+module.exports = {
+    cookieSecret: 'myblog',
+    db: 'test',
+    host: 'localhost',
+    port: 27017
+};
